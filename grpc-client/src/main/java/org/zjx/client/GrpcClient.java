e03b25e7c7baf938b4dd717dc51005286ff0af5b
zx842746244 <842746244@qq.com>
2021-07-26 22:40:28 +0800

    创建class

---
+++ grpc-client/src/main/java/org/zjx/client/GrpcClient.java
@@ -0,0 +1,11 @@
+package org.zjx.client;
+
+/**
+ * @Description:
+ * @Program: grpc-model
+ * @Author: zjx
+ * @Create: 2021-07-26 22:38:00
+ * @Version: 1.0
+ **/
+public class GrpcClient {
+}
---
+++ grpc-server/src/main/java/org/zjx/server/GrpcServer.java
@@ -0,0 +1,11 @@
+package org.zjx.server;
+
+/**
+ * @Description:
+ * @Program: grpc-model
+ * @Author: zjx
+ * @Create: 2021-07-26 22:39:55
+ * @Version: 1.0
+ **/
+public class GrpcServer {
+}
